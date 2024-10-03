0ac205fe7192fa39821f2678b08743a2111d3b42
Mikołaj <mikol.cicho@gmail.com>
2024-10-03 13:14:28 +0200

    id in serializers

--- StoreAPI/serializers.py
+++ StoreAPI/serializers.py
@@ -4,9 +4,9 @@ from .models import Product, Category
 class ProductSerializer(serializers.ModelSerializer):
     class Meta:
         model = Product
-        fields = ["title", "price", "description", "image", "category"]
+        fields = ["id", "title", "price", "description", "image", "category"]
 
 class CategorySerializer(serializers.ModelSerializer):
     class Meta:
         model = Category
-        fields = ["name", "image"]
+        fields = ["id", "name", "image"]
